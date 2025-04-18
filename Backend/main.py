7fc0699fba151b298c9eff8c2035c3a646489ed4
MikeleP <60198740+MikeleP@users.noreply.github.com>
2025-04-18 19:41:38 +0200

    Backend

--- Backend/main.py
+++ Backend/main.py
@@ -1,1 +1,24 @@
-print("Test2")
+from typing import Union
+
+from fastapi import FastAPI
+
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[""],
+    allow_methods=[""],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
